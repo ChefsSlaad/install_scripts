1d83b17ff0a76e9cf121b748792b3103026f729a
Marc Wagner <wagner.marc@gmail.com>
2017-09-22 20:58:42 +0200

    added error check for hass errors

--- homeassistant/check_hass_errors.sh
+++ homeassistant/check_hass_errors.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-#HASS_LOGFILE="/home/homeassistant/.homeassistant/2017-09-19-home-assistant.log" 
 HASS_LOGDIR="/home/homeassistant/.homeassistant"
 HASS_FILENAME="home-assistant.log"
-echo "$HASS_LOGDIR"
-echo "$HASS_FILENAME"
-
 HASS_LOGFILE=${HASS_LOGDIR}/${HASS_FILENAME}
 LOG_CONDITION="Update\sof\sswitch\.[A-Za-z]*\sis\staking\sover\s10\sseconds"
-
 LOG_DATE_TIME=$(date +'%Y-%m-%d_%H:%M:%S')
-CP_LOGFILE="cp $HASS_LOGFILE $HASS_LOGDIR/$LOG_DATE_TIME-$HASS_FILENAME"
+CP_LOGFILE="cp $HASS_LOGFILE $HASS_LOGDIR/$LOG_DATE_TIME-$HASS_FILENAME" #command to backup the logfile
+
 
 NO_ERRORS=$(grep -c $LOG_CONDITION $HASS_LOGFILE)
 
-echo "$HASS_LOGFILE"
-echo "$NO_ERRORS"
-echo "$LOG_DATE_TIME"
 
 if [ $NO_ERRORS -gt "10" ]
 then    echo "error found"
