18ffc3e527765d0757726f2a55a1e71a266d4894
Marc Wagner <wagner.marc@gmail.com>
2017-12-16 08:26:43 +0100

    added an ap_configuration script

---
+++ pi_robot_post_install.sh
@@ -0,0 +1,116 @@
+#! /bin/bash
+
+
+## hostname
+## dynamically config ap channel
+## rename ssid on the fly
+
+
+
+###################################
+#         add repositories        #
+###################################
+
+echo adding repositories
+#sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main"
+#sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+###################################
+#         Basic Update            #
+###################################
+
+echo updating system
+sudo dpkg --configure -a > /dev/null
+sudo apt-get -qq update  > /dev/null
+sudo apt-get -qq dist-upgrade > /dev/null
+sudo apt-get -qq upgrade > /dev/null
+
+###################################
+#      install and unistall apps  #
+###################################
+
+echo installing new apps
+sudo apt-get -qq install \
+    ssh git gitk gitg curl gparted \
+    dkms python3-pip nmap \
+    dnsmasq hostapd bridge-utils	
+
+sudo apt-get -y -qq remove \
+
+# stopping dsnmasq and hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
+
+
+###################################
+#      install python libraries   #
+###################################
+
+echo installing python libraries
+sudo pip3 install \
+	
+
+###################################
+#      Updating dotfiles          #
+###################################
+echo installing configuration files
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
+
+
+###################################
+#      Setting up Github          #
+###################################
+    
+mkdir ~/projects 
+
+echo setting up github
+git config --global user.email "wagner.marc@gmail.com"
+git config --global user.name "Marc Wagner"
+
+
+echo downloading the github files
+## remember to use ssh
+
+
+
+###################################
+#      Setting Up Accespoint      #
+###################################
+
+echo Setting up access point
+
+echo stopping Accessppoint # allready done
+
+#sudo systemctl stop dnsmasq
+#sudo systemctl stop hostapd
+
+echo replacing configuration files
+sudo cp projects/ap_config/dhcpd.conf /etc/dhcpcd.conf
+sudo cp projects/ap_config/dnsmasq.conf /etc/dnsmasq.conf
+sudo cp projects/ap_config/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp projects/ap_config/hostapd /etc/default/hostapd
+sudo cp projects/ap_config/sysctl.conf /etc/sysctl.conf
+sudo cp projects/ap_config/iptables.ipv4.nat /etc/iptables.ipv4.nat
+sudo cp procjects/ap_config/interfaces /etc/network/interfaces
+
+
+sudo brctl addbr br0
+sudo brctl addif br0 eth0
+
+
+
+
+
+
+
+    
+###################################
+#      cleaning up home dir       #
+###################################
+
+
+rm -rf python_games/
+
+
+
+
