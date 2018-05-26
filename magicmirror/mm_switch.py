3e34b31cfb3cef9ff0b213b3b4f795a7e4daeecb
Marc Wagner <wagner.marc@gmail.com>
2018-05-26 22:12:17 +0200

    working on switch.py

--- magicmirror/mm_switch.py
+++ magicmirror/mm_switch.py
@@ -5,9 +5,9 @@ from time import sleep
 import paho.mqtt.client as mqtt
 from time import time, sleep
 
-mirror_cmd_topic   = 'home/hall/mirror/set'
-motion_state_topic    = "home/hall/motion_sensor"
+mirror_cmd_topic   = "home/hall/mirror/set"
 mirror_state_topic = "home/hall/mirror"
+motion_state_topic = "home/hall/motion_sensor"
 mqtt_server = '192.168.1.10'
 client_name = 'hall_monitor'
 
@@ -26,27 +26,24 @@ class mqtt_handler:
         self.on_connect = None
               
     def __connect(self):
-        try:
-            self._client.connect(self.server)
-            self._client.subscribe(self.topic)
-            self._client.on_message = self.on_connect 
-            self.connected = True
-        except OSError: 
-            self.connected = False
-            
+        self._client.connect(self.server)
+        self._client.subscribe(self.topic)
+        self._client.on_message = self.on_connect 
+        print('connecting to {} subscribing to {}'.format(self.server, self.topic))
+        self.connected = True
+        self._client.loop_start()
+
     def connect_and_subscribe(self, server, callback, topic):
         self.server = server
         self.topic  = topic
         self.on_connect = callback
         self.__connect()
-    
+
     def send_message(self, topic, message):
         print('sending ', message, ' to ', topic)
-        try: 
-            self._client.publish(topic, message)
-            self.connected = True
-        except OSError:
-            self.connected = False
+        self._client.publish(topic, message)
+        self.connected = True
+        print('message succesfully sent')
 
     def check_connection(self):
         if not self.connected:
@@ -63,19 +60,24 @@ class magic_mirror:
         self.motion_tpc     = pir_tpc
         self.mirror_tpc     = mirror_tpc
         self.mirror_cmd_tpc = mirror_cmd_tpc
-        self.state          = 'ON'
-        self.motion         = 'OFF'
+        self.state          = "ON"
+        self.autonomous     = True # autonomous: respond to pir or not
+        self.motion         = "OFF"
         self.last_motion_tm = time()
-        
+        self.last_switched  = time() 
+        print('topics: motion: {}, mirror {} mirror_cmd {}'.format(self.motion_tpc, self.mirror_tpc, self.mirror_cmd_tpc))
+
+
     def __str__(self):
-        return 'monitor state: {} motion: {} last motion {}'.format(self.state, self.motion, round(self.last_motion_tm,0))
+        states = (self.state, self.motion, self.autonomous, round(time() - self.last_motion_tm), round(time() - self.last_switched))
+        return 'monitor state: {} motion: {} auto {}  since last motion {} since last switched {}'.format(*states)
 
     def switch_monitor_on_off(self, value):
 
         if value:
-            self.state = 'ON'
+            self.state = "ON"
         else:
-            self.state = 'OFF'
+            self.State = "OFF" 
         if self.monitor.value != value:
             self.monitor.value = value
             return True
@@ -83,32 +85,38 @@ class magic_mirror:
             return False
 
     def toggle_monitor(self):
-        self.switch_monitor_on_off(not self.monitor.value)
+        self.switch_monitor_on_off(self.state != 'ON')
 
     def monitor_on(self):
         self.switch_monitor_on_off(True)
-        
+ 
     def monitor_off(self):
         self.switch_monitor_on_off(False)
-        
+
     def check_motion(self, change_monitor = True, delay = 300):
+        delay_time = time() - delay
         if self.motionsensor.motion_detected:
             self.motion = 'ON'
             self.last_motion_tm = time()
-        elif (time() - self.last_motion_tm) <  delay:
+        elif self.last_motion_tm <  delay_time:
             self.motion = 'OFF'
-        print('last motion', round(self.last_motion_tm), 'since last motion' , round(time() - self.last_motion_tm)) 
-        
-        if change_monitor:
-            self.switch_monitor_on_off(self.motionsensor.value)            
+
+        if self.last_switched < delay_time:
+            self.autonomous = True
+
+        if change_monitor and self.autonomous:
+            self.switch_monitor_on_off(self.motion == 'ON')
 
     def on_message(self, client, usrdata, message):
-        payload = str(message.payload)
+        payload = (message.payload).decode("utf-8")
         print('topic', message.topic, 'payload', payload)
-        if payload == 'ON':
+        self.last_switched = time()
+        self.autonomous = False
+        if payload:
             self.monitor_on()
         else: 
             self.monitor_off()
+
 
 def initiate(client_name, mqtt_server, pir_tpc, mirror_tpc, mirror_cmd_tpc):
     mirror = magic_mirror(pir_tpc, mirror_tpc, mirror_cmd_tpc)
@@ -117,21 +125,23 @@ def initiate(client_name, mqtt_server, pir_tpc, mirror_tpc, mirror_cmd_tpc):
     return (mirror, mqtt_client)
 
 def run(mirror, mqtt_client):
-    while True:
-        mqtt_client.check_connection()
-        motion_status = mirror.motion
-        mirror_status = mirror.state
-        mirror.check_motion()
-        if motion_status != mirror.motion:
-            mqtt_client.send_message(mirror.motion_tpc, mirror.motion)
-        if mirror_status != mirror.state:
-            print(mirror)
-            mqtt_client.send_message(mirror.mirror_tpc, mirror.state)
-        sleep(1)
+    mqtt_client.check_connection()
+    motion_status = mirror.motion
+    mirror_status = mirror.state
+    mirror.check_motion()
+    sleep(1)
+    print('mirror', mirror_status, 'motion', motion_status)
+    print(mirror)
+    if mirror_status != mirror.state:
+        mqtt_client.send_message(mirror.mirror_tpc, mirror.state)
+    if motion_status != mirror.motion:
+        mqtt_client.send_message(mirror.motion_tpc, mirror.motion)
+
         
 
         
 if __name__ == "__main__":
     mirror, client = initiate(client_name, mqtt_server, motion_state_topic, mirror_state_topic, mirror_cmd_topic)
-    run(mirror, client)
+    while True:
+        run(mirror, client)
 
