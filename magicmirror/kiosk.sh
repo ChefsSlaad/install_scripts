3137b7578a6f2d6937884c5141ab3e9813d31fba
Marc Wagner <wagner.marc@gmail.com>
2019-01-09 21:27:51 +0100

    updates to mirror control scripts

--- magicmirror/kiosk.sh
+++ magicmirror/kiosk.sh
@@ -4,10 +4,14 @@ xset s noblank
 xset s off
 xset -dpms
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://de-wagnertjes.duckdns.org/lovelace/hall &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://de-wagnertjes.duckdns.org/lovelace/hall?kiosk &
 
 
+while true; do
+   xdotool key f5 
+   sleep 21600 #six hours
+done
 
