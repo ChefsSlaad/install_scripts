679a7b750a2574a3f58e874b2d43ad2ec9a953ef
Marc <wagner.marc@gmail.com>
2023-12-02 13:42:27 +0100

    Update css patch path

--- foundryvtt/css_patch.sh
+++ foundryvtt/css_patch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CSS_PATH="resources/app/public/css/foundry2.css"
-CSS_URL="https://raw.githubusercontent.com/ChefsSlaad/install_scripts/master/foundryvtt/css_patch.css"
+CSS_URL="https://github.com/ChefsSlaad/install_scripts/blob/ec4adc02eda799dd8e3a9fdae4fe8053d916e91c/foundryvtt/css_patch.css"
 
 log "Downloading CSS from $CSS_URL"
 curl -s -o /tmp/foundry_login.css $CSS_URL
